6aa32b3c3d418ab5ddeb988cfbac56ea3a2c4e82
dudnyko <117224090+dudnyko@users.noreply.github.com>
2024-09-12 00:33:45 +0300

    Add files via upload

---
+++ Car.cpp
@@ -0,0 +1,175 @@
+﻿#include <iostream>
+#include <cmath>
+
+class NotEnoughTankCapacityException {};
+class NotEnoughFuelException {};
+
+
+class Location {
+private:
+    int x;
+    int y;
+
+public:
+    Location(int x = 0, int y = 0) {
+        this->x = x;
+        this->y = y;
+    }
+
+    int getX() const {
+        return this->x;
+    }
+
+    int getY() const {
+        return this->y;
+    }
+
+    double distance(const Location& location) const {
+        return hypot(this->x - location.x, this->y - location.y);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Location& location);
+};
+
+class Car {
+private:
+    std::string model;
+    double tankCapacity;
+    double fuelAmount;
+    double mileage;
+    double fuelConsumption;
+    Location location;
+
+public:
+    Car(const std::string& model, double tankCapacity, double fuelConsumption) {
+        this->model = model;
+        this->tankCapacity = tankCapacity;
+        this->fuelAmount = 0.0;
+        this->mileage = 0.0;
+        this->fuelConsumption = fuelConsumption;
+        this->location = Location();
+    }
+
+    const std::string& getModel() const {
+        return this->model;
+    }
+
+    double getTankCapacity() const {
+        return this->tankCapacity;
+    }
+
+    double getFuelAmount() const {
+        return this->fuelAmount;
+    }
+
+    double getMileage() const {
+        return this->mileage;
+    }
+
+    const Location& getLocation() const {
+        return this->location;
+    }
+
+    void refuel(double fuel) {
+        if (this->fuelAmount + fuel > this->tankCapacity) {
+            NotEnoughTankCapacityException obj;
+            throw obj;
+        }
+        else {
+            this->fuelAmount = this->fuelAmount + fuel;
+        }
+    }
+
+    void refuelUntilFull() {
+        this->fuelAmount = this->tankCapacity;
+    }
+
+    void move(const Location& newLocation) {
+        double distance = this->location.distance(newLocation);
+        double fuelRequired = this->fuelConsumption * distance;
+
+        if (this->getFuelAmount() - fuelRequired > 0) {
+            this->location = newLocation;
+            this->fuelAmount -= fuelRequired;
+            this->mileage += distance;
+        }
+        else {
+            NotEnoughFuelException obj;
+            throw obj;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Car& car);
+};
+
+std::ostream& operator<<(std::ostream& out, const Location& location) {
+    out << "(" << location.x << ", " << location.y << ")";
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const Car& car) {
+    out << car.model << ": " << std::endl;
+    out << "  - fuel: " << car.fuelAmount << "/" << car.tankCapacity << std::endl;
+    out << "  - mileage: " << car.mileage << std::endl;
+    out << "  - location: " << car.location << std::endl;
+    return out;
+}
+
+int main() {
+    Car toyota("Toyota", 60.0, 1.3);
+
+    std::cout << toyota << std::endl;
+
+    // Toyota:
+    //   - fuel: 0/60
+    //   - mileage: 0
+    //   - location: (0, 0)
+
+    toyota.refuel(30.0);
+    std::cout << toyota << std::endl;
+
+    // Toyota:
+    //   - fuel: 30/60
+    //   - mileage: 0
+    //   - location: (0, 0)
+
+    toyota.move(Location(10, 4));
+    std::cout << toyota << std::endl;
+
+    // Toyota:
+    //   - fuel: 15.9986/60
+    //   - mileage: 10.7703
+    //   - location: (10, 4)
+
+    toyota.move(Location());
+    std::cout << toyota << std::endl;
+
+    // Toyota:
+    //   - fuel: 1.99714/60
+    //   - mileage: 21.5407
+    //   - location: (0, 0)
+
+    try {
+        toyota.move(Location(10, 8));
+    }
+    catch (NotEnoughFuelException exc) {
+        std::cout << "Not enough fuel..." << std::endl;
+    }
+
+    try {
+        toyota.refuel(100.0);
+    }
+    catch (NotEnoughTankCapacityException exc) {
+        std::cout << "Not enough tank capacity..." << std::endl;
+    }
+
+    toyota.refuelUntilFull();
+    std::cout << toyota << std::endl;
+
+    // Toyota:
+    //   - fuel: 60/60
+    //   - mileage: 21.5407
+    //   - location: (0, 0)
+
+    return 0;
+}
---
+++ Unit.cpp
@@ -0,0 +1,60 @@
+﻿// Юніт
+// Юніт має назву, колькість поінтів здоров'я та показник атаки
+// Юніт може атакувати ворога
+// Юніт якого атакують, завжди контратакує на половину сили
+// Мертвих юнітів атакувати неможна
+// Показник здоров'я юніта не може бути менший за 0 ;)
+
+#include <iostream>
+
+class UnitIsDeadException {};
+
+class Unit {
+private:
+    std::string title;
+    int hp;
+    int maxHP;
+    int damage;
+
+public:
+    Unit(const std::string& title, int maxHP, int damage) {
+        // your code here :)
+    }
+
+    // your getters/setters here :)
+    // E.g:
+    //
+    // const std::string& getTitle() const {
+    //     return this->title;
+    // }
+    //
+    // etc...
+
+    void attack(Unit& enemy) {
+        // your code here :)
+    }
+
+    void counterAttack(Unit& enemy) {
+        // your code here :)
+    }
+};
+
+std::ostream& operator<<(std::ostream& out, const Unit& unit) {
+    // your code here :)
+    // Output example: <Title>:: hp(<hp>/<maxHP>), dmg(<damage>)
+}
+
+int main() {
+    Unit knight("Knight", 100, 20);
+    Unit barbarian("Barbarian", 80, 30);
+
+    std::cout << knight << std::endl; // Knight:: hp(100/100), dmg(20)
+    std::cout << barbarian << std::endl; // Barbarian:: hp(80/80), dmg(30)
+
+    knight.attack(barbarian);
+
+    std::cout << knight << std::endl; // Knight:: hp(85/100), dmg(20)
+    std::cout << barbarian << std::endl; // Barbarian:: hp(60/80), dmg(30)
+
+    return 0;
+}
